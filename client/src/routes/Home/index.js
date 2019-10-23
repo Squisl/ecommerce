ae846fbbc495d56022cb998a1a0cbe97d606a57f
liew <joeyliew@tutanota.com>
2019-10-23 20:00:09 +0200

    swap carousel images

--- client/src/routes/Home/index.js
+++ client/src/routes/Home/index.js
@@ -2,21 +2,25 @@ import React from "react"
 import styles from "./styles.module.css"
 import Carousel from "../../components/Carousel/Carousel"
 import CarouselSlide from "../../components/CarouselSlide/CarouselSlide"
+import Bonsai_1 from "../../assets/images/bonsai_1.jpg"
+import Bonsai_2 from "../../assets/images/bonsai_2.jpg"
+import Bonsai_3 from "../../assets/images/bonsai_3.jpg"
+import Bonsai_4 from "../../assets/images/bonsai_4.jpg"
 
 const Home = () => {
   return (
     <div className={styles.home}>
       <Carousel>
-        <CarouselSlide image="https://images.unsplash.com/photo-1445205170230-053b83016050?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2551&q=80">
+        <CarouselSlide image={Bonsai_1}>
           <p>Carousel</p>
         </CarouselSlide>
-        <CarouselSlide image="https://images.unsplash.com/photo-1470309864661-68328b2cd0a5?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80">
+        <CarouselSlide image={Bonsai_2}>
           <p>Carousel</p>
         </CarouselSlide>
-        <CarouselSlide image="https://images.unsplash.com/photo-1445205170230-053b83016050?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2551&q=80">
+        <CarouselSlide image={Bonsai_3}>
           <p>Carousel</p>
         </CarouselSlide>
-        <CarouselSlide image="https://images.unsplash.com/photo-1445205170230-053b83016050?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2551&q=80">
+        <CarouselSlide image={Bonsai_4}>
           <p>Carousel</p>
         </CarouselSlide>
       </Carousel>
